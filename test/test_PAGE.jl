8a08919a39db797ada460d978a57a74f9d305761
lrennels <lrennels@users.noreply.github.com>
2021-06-16 00:09:20 +0000

    Format files using DocumentFormat

--- test/test_PAGE.jl
+++ test/test_PAGE.jl
@@ -3,28 +3,28 @@ using DelimitedFiles
 
 @testset "PAGE" begin
 
-@testset "API" begin
+    @testset "API" begin
 
-    m = MimiIWG.get_model(PAGE, MimiIWG.scenarios[1])
-    run(m)
+        m = MimiIWG.get_model(PAGE, MimiIWG.scenarios[1])
+        run(m)
 
-    md1 = MimiIWG.get_marginaldamages(PAGE, MimiIWG.scenarios[1])
-    md2 = MimiIWG.get_marginaldamages(PAGE, MimiIWG.scenarios[1], regional = true)
+        md1 = MimiIWG.get_marginaldamages(PAGE, MimiIWG.scenarios[1])
+        md2 = MimiIWG.get_marginaldamages(PAGE, MimiIWG.scenarios[1], regional=true)
 
-    scc1 = MimiIWG.compute_scc(PAGE, MimiIWG.scenarios[1])
-    scc2 = MimiIWG.compute_scc(PAGE, MimiIWG.scenarios[1], domestic = true)
-    @test scc2 < scc1
+        scc1 = MimiIWG.compute_scc(PAGE, MimiIWG.scenarios[1])
+        scc2 = MimiIWG.compute_scc(PAGE, MimiIWG.scenarios[1], domestic=true)
+        @test scc2 < scc1
 
-    tmp_dir = joinpath(@__DIR__, "tmp")
-    MimiIWG.run_scc_mcs(PAGE, trials=2, output_dir = tmp_dir, domestic=true)
-    rm(tmp_dir, recursive=true)
+        tmp_dir = joinpath(@__DIR__, "tmp")
+        MimiIWG.run_scc_mcs(PAGE, trials=2, output_dir=tmp_dir, domestic=true)
+        rm(tmp_dir, recursive=true)
 
     # test the drop discontinuities flag set to `true`
-    tmp_dir = joinpath(@__DIR__, "tmp")
-    MimiIWG.run_scc_mcs(PAGE, trials=2, output_dir = tmp_dir, domestic=true, drop_discontinuities = true)
-    rm(tmp_dir, recursive=true)
+        tmp_dir = joinpath(@__DIR__, "tmp")
+        MimiIWG.run_scc_mcs(PAGE, trials=2, output_dir=tmp_dir, domestic=true, drop_discontinuities=true)
+        rm(tmp_dir, recursive=true)
 
-end
+    end
 
 @testset "Deterministic SCC validation" begin 
 
@@ -33,7 +33,7 @@ end
 
     _atol = 0.01    # one cent
 
-    scenario_convert_flip = Dict([v=>k for (k,v) in MimiIWG.page_scenario_convert]) # need to convert scenario names in the other direction from the validation data files
+    scenario_convert_flip = Dict([v => k for (k, v) in MimiIWG.page_scenario_convert]) # need to convert scenario names in the other direction from the validation data files
 
     for f in files
         validation_data = readdlm(joinpath(validation_dir, f), ',')
